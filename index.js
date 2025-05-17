5ee710bec0702e3aceaf5e3e39944e4d50b624ba
Murugan-kombaiya <murugan.webbuilder@gmail.com>
2025-05-18 00:13:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("Backend Project");
 console.log("This also a main line of this code ")
 console.log("I am murugan and i am a software developer");
+
+console.log("I am from main branch")
